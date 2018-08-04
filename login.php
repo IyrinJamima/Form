5036b050145cc7481a6ac5b5dc06bc161edf8db8
IyrinJamima <iyrinjami810@gmail.com>
2018-08-04 19:03:42 +0530

    newlogin

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+   <head>
+      <title>Login Page</title>
+      </head>
+               <form action ="" method = "post" enctype="multipart/form-data" align="center">
+                  <label>UserName  :</label><input type = "text" name = "email_id"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password"/><br/><br />
+                  <input type="submit" name="submit" value="submit">
+               </form>
+</html>
+
+
