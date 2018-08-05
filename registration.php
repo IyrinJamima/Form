23f25c05727f82c4a65631ee4b6b7a801974546d
IyrinJamima <iyrinjami810@gmail.com>
2018-08-05 21:34:41 +0530

    regform

--- registration.php
+++ registration.php
@@ -3,5 +3,10 @@
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);*/
 include('db.php');
+if(isset($_REQUEST['submit'])!=''){
+        if($_REQUEST['name']=='' || $_REQUEST['email']=='' || $_REQUEST['password']==''){
+            echo "please fill the empty field.";
+        }else{
+            $sql="INSERT INTO login (name,email,password) values ('".$_REQUEST['name']."','".$_REQUEST['email']."', '".$_REQUEST['password']."')";
 ?>
 
