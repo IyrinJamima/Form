867a23d4494e28d5865fc363100b27b23673e66c
IyrinJamima <iyrinjami810@gmail.com>
2018-08-05 20:54:52 +0530

    registration

--- registration.php
+++ registration.php
@@ -3,18 +3,20 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL); 
 include('db.php');
- if(isset($_POST['save'])){
+    if(isset($_POST['submit'])){
             $sql = "INSERT INTO login (username,password) VALUES ('".$_POST['username']."','".$_POST['password']."')";
-            $result = mysqli_query($db,$sql);
-            }
+            $res = mysqli_query($db,$sql);
+       
+}
+
 ?>
 <html>
    <head>
       <title>Login Page</title>
       </head>
       <body>
-               <form action = "registration.html" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "email_id"/><br /><br />
+               <form action = "reg.php" method = "post" align = "center">
+                  <label>UserName  :</label><input type = "text" name = "username"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password"/><br/><br />
                   <input type="submit" name="submit" value="login" onclick ="javascript:login()">
                </form>
@@ -23,7 +25,7 @@ include('db.php');
 <script>
     function login(){
           if(confirm('sure to login')){
-            window.location.href ='registration.html';
+            window.location.href ='reg.php';
       }
    }
 </script>  
