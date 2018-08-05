0b26f0aa0c4f041f08e6410f77d8b4f7bc382551
IyrinJamima <iyrinjami810@gmail.com>
2018-08-05 21:08:56 +0530

    reg

--- registration.php
+++ registration.php
@@ -1,31 +1,7 @@
 <?php
-ini_set('display_errors', 1); 
+/*ini_set('display_errors', 1); 
 ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL); 
+error_reporting(E_ALL);*/
 include('db.php');
-    if(isset($_POST['submit'])){
-            $sql = "INSERT INTO login (username,password) VALUES ('".$_POST['username']."','".$_POST['password']."')";
-            $res = mysqli_query($db,$sql);
-       
-}
-
 ?>
-<html>
-   <head>
-      <title>Login Page</title>
-      </head>
-      <body>
-               <form action = "reg.php" method = "post" align = "center">
-                  <label>UserName  :</label><input type = "text" name = "username"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password"/><br/><br />
-                  <input type="submit" name="submit" value="login" onclick ="javascript:login()">
-               </form>
-</body>
-</html>
-<script>
-    function login(){
-          if(confirm('sure to login')){
-            window.location.href ='reg.php';
-      }
-   }
-</script>  
+
